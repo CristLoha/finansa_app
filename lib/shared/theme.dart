9aa96f63d7e2d6ab9a56f5f304fd29ed38ca4063
Christ Henry Loha <cristloha2205@gmail.com>
2021-10-22 12:07:09 +0800

    update

---
+++ lib/shared/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30.0;
+
+Color primaryColor = Color(0xff5F59E1);
+Color secondaryColor = Color(0xff38ABBE);
+Color backgroundColor1 = Color(0xffFFFFFF);
+Color backgroundColor2 = Color(0xffFAFAFA);
+Color primaryTextColor = Color(0xff111111);
+Color secondaryTextColor = Color(0xffB6B6B6);
+Color transparentColor = Colors.transparent;
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryTextColor,
+);
+
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: secondaryTextColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: backgroundColor1,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
